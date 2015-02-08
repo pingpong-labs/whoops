368b85f3f859e2926d3861bc414a31740d6ebdb7
Andrei <AndreiLunga@users.noreply.github.com>
2015-02-08 18:19:50 +0200

    Update WhoopsHandler.php

--- src/Pingpong/Whoops/WhoopsHandler.php
+++ src/Pingpong/Whoops/WhoopsHandler.php
@@ -1,19 +1,44 @@
 <?php namespace Pingpong\Whoops;
 
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler;
+use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
-class WhoopsHandler {
+class ExceptionHandler extends Handler
+{
 
-	/**
-	 * @param  mixed $handler 
-	 * @return void          
-	 */
-	public static function handle($handler)
-	{
-        if($handler instanceof PrettyPageHandler)
-        {
-            $handler->addResourcePath(__DIR__ . '/Resources');
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException'
+    ];
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * When debugging is enabled, we make the error pretty using Whoops
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception                $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        if (config('app.debug')) {
+            $whoops = new Run;
+            $whoops->pushHandler($request->ajax() ? new JsonResponseHandler : new PrettyPageHandler);
+            $whoops->allowQuit(false);
+            $whoops->writeToOutput(false);
+
+            return response($whoops->handleException($e), $whoops->sendHttpCode());
         }
-	}
+
+        return parent::render($request, $e);
+    }
 
 }
